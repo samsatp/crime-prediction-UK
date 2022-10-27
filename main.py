96809c11bca222cb58dac7fd0e962c29b7574f08
Sathianpong Trangcasanchai <sathianpong.t@deverhood.com>
2022-10-27 17:37:41 +0300

    add server to main

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
-from data_prep import create_base_map
+from VIZ.app import server
 
-import argparse
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument('--func', type=str, required=True)
-args = parser.parse_args()
-
-if __name__ == '__main__':
-    if args.name.lower() == 'createbasemap':
-        create_base_map.create_base_map()
